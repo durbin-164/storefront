470bc4af0e9183ad9dfc0f04d71424bd78d67f84
Masud Rana <masud.rana@infolytx.com>
2022-12-18 17:47:11 +0600

    update code

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from store.models import Product
 
-# Create your views here.
+
 
 def say_hello(request):
-    x=1
-    y = 2
-    return render(request, 'hello.html', {'name': "Masud"})
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product)
+
+    return render(request, 'hello.html', {'name': 'Mosh'})
